f59f22759f22ee25b3190745ff55479e9b5b78b1
MightyJabe <98521801+MightyJabe@users.noreply.github.com>
2023-12-30 01:41:30 +0200

    Merge pull request #5
    * added better testing

--- e2e.py
+++ e2e.py
@@ -1,57 +1,42 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import NoSuchElementException
+
+# The URL of your application's homepage
+HOMEPAGE_URL = 'http://localhost:5000'  # Replace with the correct URL
+
+# XPaths to the game elements on the homepage
+GAME_LINKS_XPATH = [
+    '//a[@href="/memory-game"]',
+    '//a[@href="/guess-game"]',
+    '//a[@href="/currency-roulette-game"]'
+]
 
 
-def test_scores_service():
-    # get the URL from the user
-    url = input("Please enter your URL: ")
+def test_games_links(driver, game_links_xpath):
+    success = True
 
-    # open the URL in a web browser using Selenium WebDriver
-    driver = webdriver.Chrome()  # You may need to download the appropriate webdriver for your browser
-    print(f"Opening URL: {url}")
-    driver.get(url)
+    for game_xpath in game_links_xpath:
+        try:
+            game_link = driver.find_element(By.XPATH, game_xpath)
+            game_link.click()
+            # Here you could add more checks, e.g., for page content
+            driver.back()  # Go back to the homepage
+        except NoSuchElementException:
+            print(f"Game link with XPath '{game_xpath}' not found on the homepage.")
+            success = False
 
-    try:
-        # Wait up to 10 seconds for the element to be present
-        print("Waiting for the 'score' element to be present...")
-        score = WebDriverWait(driver, 10).until(
-            ec.presence_of_element_located((By.ID, "score"))
-        )
-
-        score_value = int(score.text)
-        print(f"Score value found: {score_value}")
-
-        # check that it is a number between 1 and 1000
-        if 1 <= score_value <= 1000:
-            print("Test passed! Score is within the expected range.")
-            return True
-        else:
-            print("Test failed! Score is outside the expected range.")
-            return False
-
-    except NoSuchElementException:
-        print("Element with id 'score' not found.")
-        return False
-    finally:
-        # Close the browser after the test
-        print("Closing the browser.")
-        driver.quit()
+    return success
 
 
-def main_function():
-    # this function will call the test_scores_service() function, and return -1
-    # as an os exit code if the tests failed, or 0 if the tests passed.
-    if test_scores_service():
-        return 0
-    else:
-        return -1
-
-
-# Call the main function
 if __name__ == "__main__":
-    exit_code = main_function()
-    print(f"Exit code: {exit_code}")
-    exit(exit_code)
+    web_driver = webdriver.Chrome()  # Replace with the browser you're using
+    try:
+        web_driver.get(HOMEPAGE_URL)
+        result = test_games_links(web_driver, GAME_LINKS_XPATH)
+        if result:
+            print("All game links were successfully reached from the homepage.")
+        else:
+            print("Some game links could not be reached from the homepage.")
+    finally:
+        web_driver.quit()  # Make sure to quit the driver to close the browser window
